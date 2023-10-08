dd1d8234797905d047b47069724169452c6e792d
Pushkar Kumar Gautam <kumargautampushkar@gmail.com>
2023-10-09 01:16:04 +0530

    Border rounded, working and functional html page

---
+++ script.js
@@ -0,0 +1,15 @@
+let x= document.getElementById("login");
+let y= document.getElementById("register");
+let z=document.getElementById("btn");
+
+function login(){
+    x.style.left="-400px" ;
+    y.style.left="50px" ;
+    z.style.left= "110.4px";
+}
+
+function register(){
+    x.style.left="50px" ;
+    y.style.left="450px" ;
+    z.style.left= "0px";
+}
